044b4ccd49d058c48f4c4087fce9f3bb7272ef4a
Tobias Klug <tbs.klg@gmail.com>
2023-12-25 09:51:02 +0100

    Solve day 15 part one

---
+++ day-15/src/lib.rs
@@ -0,0 +1,30 @@
+#[test]
+fn should_calculate_hash() {
+    let input = "HASH";
+
+    let hash = hash_from(input);
+
+    assert_eq!(hash, 52);
+}
+
+#[test]
+fn should_calculate_sum_of_seqence() {
+    let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
+    let hash = hash_sum(input);
+
+    assert_eq!(hash, 1320);
+}
+
+pub fn hash_sum(input: &str) -> u64 {
+    input.split(',').map(|x| hash_from(x)).sum()
+}
+
+fn hash_from(input: &str) -> u64 {
+    input.chars().fold(0, |acc, curr| {
+        if curr == '\n' {
+            return acc;
+        }
+        ((acc + curr as u64) * 17) % 256
+    })
+}
---
+++ day-15/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs;
+
+fn main() {
+    let path = std::path::Path::new("day-15/src/input.txt");
+    let input = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    println!("Trying to solve part 1...");
+    let hash_sum = day_15::hash_sum(input.to_string().as_str());
+    println!("Solution is: {}", hash_sum);
+}
