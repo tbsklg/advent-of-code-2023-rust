f5cf39476fe2b2285dc506bb1d7dab29e740034c
Tobias Klug <tbs.klg@gmail.com>
2023-12-26 14:20:19 +0100

    Clippy fix

--- day-15/src/lib.rs
+++ day-15/src/lib.rs
@@ -1,23 +1,5 @@
 use std::collections::HashMap;
 
-#[test]
-fn should_calculate_hash() {
-    let input = "HASH";
-
-    let hash = hash_from(input);
-
-    assert_eq!(hash, 52);
-}
-
-#[test]
-fn should_calculate_sum_of_seqence() {
-    let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
-
-    let hash = hash_sum(input);
-
-    assert_eq!(hash, 1320);
-}
-
 pub fn hash_sum(input: &str) -> u64 {
     input.split(',').map(hash_from).sum()
 }
@@ -39,10 +21,8 @@ pub fn focusing_power(input: &str) -> u64 {
                     let updated_lenses = update_or_insert(lenses.to_vec(), curr);
                     acc.insert(box_index, updated_lenses);
                 }
-            } else {
-                if curr.contains('=') {
-                    acc.insert(box_index, vec![curr.to_string()]);
-                }
+            } else if curr.contains('=') {
+                acc.insert(box_index, vec![curr.to_string()]);
             }
             acc
         },
@@ -112,4 +92,22 @@ fn hash_from(input: &str) -> u64 {
         }
         ((acc + curr as u64) * 17) % 256
     })
+}
+
+#[test]
+fn should_calculate_hash() {
+    let input = "HASH";
+
+    let hash = hash_from(input);
+
+    assert_eq!(hash, 52);
+}
+
+#[test]
+fn should_calculate_sum_of_seqence() {
+    let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
+    let hash = hash_sum(input);
+
+    assert_eq!(hash, 1320);
 }
