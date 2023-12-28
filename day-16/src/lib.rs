ab47bd6f34e0e262294c8c18786e7544b4d9808a
Tobias Klug <tbs.klg@gmail.com>
2023-12-28 06:45:11 +0100

    Solve day 16 part one

---
+++ day-16/src/lib.rs
@@ -0,0 +1,77 @@
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Dir {
+    Right,
+    Left,
+    Down,
+    Up,
+}
+
+pub fn tiles(input: Vec<&str>) -> u64 {
+    let mut pos: Vec<((i32, i32), Dir)> = vec![((0, 0), Dir::Right)];
+    let mut visited: Vec<((i32, i32), Dir)> = vec![((0, 0), Dir::Right)];
+
+    while !pos.is_empty() {
+        let ((r, c), dir) = pos.remove(0);
+
+        let curr = input
+            .get(r as usize)
+            .and_then(|row| row.chars().nth(c as usize));
+
+        if curr.is_none() {
+            pos.remove(0);
+            break;
+        }
+
+        let next = next_pos(((r, c), dir), curr.unwrap())
+            .into_iter()
+            .filter(|((r, c), _)| in_bounds(input.clone(), *r, *c))
+            .filter(|((r, c), dir)| !visited.contains(&((*r, *c), *dir)))
+            .collect::<Vec<_>>();
+
+        visited.extend(next.clone());
+
+        pos.extend(next);
+    }
+
+    let mut energized = HashSet::new();
+
+    for ((r, c), _) in visited {
+        energized.insert((r, c));
+    }
+
+    energized.len() as u64
+}
+
+fn in_bounds(input: Vec<&str>, r: i32, c: i32) -> bool {
+    r >= 0 && c >= 0 && r < input.len() as i32 && c < input[0].len() as i32
+}
+
+fn next_pos(dir: ((i32, i32), Dir), sign: char) -> Vec<((i32, i32), Dir)> {
+    let ((r, c), dir) = dir;
+
+    match (dir, sign) {
+        (Dir::Up, '/') => vec![((r, c + 1), Dir::Right)],
+        (Dir::Down, '/') => vec![((r, c - 1), Dir::Left)],
+        (Dir::Right, '/') => vec![((r - 1, c), Dir::Up)],
+        (Dir::Left, '/') => vec![((r + 1, c), Dir::Down)],
+        (Dir::Up, '\\') => vec![((r, c - 1), Dir::Left)],
+        (Dir::Down, '\\') => vec![((r, c + 1), Dir::Right)],
+        (Dir::Right, '\\') => vec![((r + 1, c), Dir::Down)],
+        (Dir::Left, '\\') => vec![((r - 1, c), Dir::Up)],
+        (Dir::Up, '|') => vec![((r - 1, c), dir)],
+        (Dir::Down, '|') => vec![((r + 1, c), dir)],
+        (Dir::Right, '|') => vec![((r + 1, c), Dir::Down), ((r - 1, c), Dir::Up)],
+        (Dir::Left, '|') => vec![((r + 1, c), Dir::Down), ((r - 1, c), Dir::Up)],
+        (Dir::Up, '-') => vec![((r, c - 1), Dir::Left), ((r, c + 1), Dir::Right)],
+        (Dir::Down, '-') => vec![((r, c - 1), Dir::Left), ((r, c + 1), Dir::Right)],
+        (Dir::Right, '-') => vec![((r, c + 1), dir)],
+        (Dir::Left, '-') => vec![((r, c - 1), dir)],
+        (Dir::Up, '.') => vec![((r - 1, c), dir)],
+        (Dir::Down, '.') => vec![((r + 1, c), dir)],
+        (Dir::Right, '.') => vec![((r, c + 1), dir)],
+        (Dir::Left, '.') => vec![((r, c - 1), dir)],
+        _ => vec![],
+    }
+}
---
+++ day-16/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs;
+
+fn main() {
+    let path = std::path::Path::new("day-16/src/input.txt");
+    let input = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    println!("Trying to solve part 1...");
+    let tiles = day_16::tiles(input.lines().collect());
+    println!("Solution is: {}", tiles);
+}
