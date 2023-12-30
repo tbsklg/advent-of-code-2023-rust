025291b57df9e80e219b0071409b002eb3c393e5
Tobias Klug <tbs.klg@gmail.com>
2023-12-30 06:45:11 +0100

    Solve day 16 part two

--- day-16/src/lib.rs
+++ day-16/src/lib.rs
@@ -1,17 +1,54 @@
 use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-enum Dir {
+pub enum Dir {
     Right,
     Left,
     Down,
     Up,
 }
 
-pub fn tiles(input: Vec<&str>) -> u64 {
-    let mut pos: Vec<((i32, i32), Dir)> = vec![((0, 0), Dir::Right)];
+pub fn tiles_2(input: Vec<&str>) -> u64 {
+    let mut best = 0;
+
+    for r in 0..input.len() {
+        let to_right = tiles(input.clone(), ((r as i32, 0), Dir::Right));
+
+        if to_right > best {
+            best = to_right;
+        }
+
+        let to_left = tiles(
+            input.clone(),
+            ((r as i32, input[0].len() as i32 - 1), Dir::Left),
+        );
+
+        if to_left > best {
+            best = to_left;
+        }
+    }
+
+    for c in 0..input[0].len() {
+        let to_down = tiles(input.clone(), ((0, c as i32), Dir::Down));
+
+        if to_down > best {
+            best = to_down;
+        }
+
+        let to_up = tiles(input.clone(), ((input.len() as i32 - 1, c as i32), Dir::Up));
+
+        if to_up > best {
+            best = to_up;
+        }
+    }
+
+    best
+}
+
+pub fn tiles(input: Vec<&str>, start: ((i32, i32), Dir)) -> u64 {
+    let mut pos: Vec<((i32, i32), Dir)> = vec![start];
     let mut visited: HashSet<((i32, i32), Dir)> = HashSet::new();
-    let mut energized: HashSet<(i32, i32)> = HashSet::new();
+    let mut energized: HashSet<(i32, i32)> = HashSet::from_iter(vec![start.0]);
 
     while !pos.is_empty() {
         let ((r, c), dir) = pos.remove(0);
--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -6,7 +6,14 @@ fn main() {
 
     println!("Trying to solve part 1...");
     let start = std::time::Instant::now();
-    let tiles = day_16::tiles(input.lines().collect());
+    let tiles = day_16::tiles(input.lines().collect(), ((0, 0), day_16::Dir::Right));
+    let elapsed = start.elapsed();
+    println!("Solution is: {}", tiles);
+    println!("Elapsed time: {:?}", elapsed);
+
+    println!("Trying to solve part 2...");
+    let start = std::time::Instant::now();
+    let tiles = day_16::tiles_2(input.lines().collect());
     let elapsed = start.elapsed();
     println!("Solution is: {}", tiles);
     println!("Elapsed time: {:?}", elapsed);
