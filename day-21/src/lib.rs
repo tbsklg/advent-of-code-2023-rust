16913bf5b638c8eb58574f2195b2cb6a659a25be
Tobias Klug <tbs.klg@gmail.com>
2024-01-15 08:32:26 +0100

    Solve day 21 part one

---
+++ day-21/src/lib.rs
@@ -0,0 +1,53 @@
+use std::collections::HashSet;
+
+pub fn plots(collect: Vec<&str>, steps: usize) -> usize {
+    let mut curr_pos = HashSet::from([start(collect.clone())]);
+
+    for _ in 0..steps {
+        curr_pos = next_poss(collect.clone(), curr_pos)
+            .iter()
+            .filter(|(r, c)| collect[*r].chars().nth(*c).unwrap() == '.')
+            .map(|p| *p)
+            .collect();
+    }
+
+    let unique_pos = curr_pos.iter().collect::<HashSet<_>>();
+    unique_pos.len() + 1
+}
+
+type Pos = (usize, usize);
+
+const START: char = 'S';
+
+fn start(garden: Vec<&str>) -> Pos {
+    garden
+        .iter()
+        .enumerate()
+        .find(|(_, l)| l.contains(START))
+        .map(|(r, l)| (r, l.find(START).unwrap()))
+        .unwrap()
+}
+
+fn next_poss(garden: Vec<&str>, poss: HashSet<Pos>) -> HashSet<Pos> {
+    poss.iter()
+        .flat_map(|p| next_pos(garden.clone(), *p))
+        .collect::<HashSet<_>>()
+}
+
+fn next_pos(garden: Vec<&str>, (r, c): Pos) -> Vec<Pos> {
+    vec![
+        (r + 1, c),
+        (r, c + 1),
+        (r.saturating_sub(1), c),
+        (r, c.saturating_sub(1)),
+    ]
+    .iter()
+    .filter(|p| in_garden(&garden, **p))
+    .map(|p| *p)
+    .collect()
+}
+
+fn in_garden(garden: &Vec<&str>, pos: Pos) -> bool {
+    let (r, c) = pos;
+    r < garden.len() && c < garden[r].len()
+}
--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::path::Path::new("day-21/src/input.txt");
+    let input = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    println!("Trying to solve part 1...");
+    let start = std::time::Instant::now();
+    let result = day_21::plots(input.lines().collect(), 64);
+    let elapsed = start.elapsed();
+    println!("Elapsed time: {:?}", elapsed);
+    println!("Solution is: {:?}", result);
 }
